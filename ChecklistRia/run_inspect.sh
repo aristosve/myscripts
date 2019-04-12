fac8e23e7cbdcac09aa344e91c4259d785ec21c7
aristosve <aristoteles.velazquez@enova.mx>
2019-04-12 16:45:15 -0500

    esta es una prueba

---
+++ ChecklistRia/run_inspect.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#INVENTORY=ruta de archivo inventario
+#HOSTS=Nombre de maquina o grupo de maquinas de inventario ansible
+#INSPEC_PROFILE=directorio del profile de Inspec
+#PORT= Puerto de acceso ssh (-p 443, -p 22)
+#CONTROLS=nombre del control a ejecutar (--controls=nombrecontrol)
+
+ansible all -i $INVENTORY --limit $HOSTS -m debug -a "var=ansible_ssh_host"| grep "ansible_ssh_host" |sed 's/\x1b\[[0-9;]*m//g'|sed 's/"//g'|awk {'print $2'} |while read -r line
+do
+   echo inspec exec $INSPEC_PROFILE -t ssh://rot@$line $PORT $PASSWORD --reporter documentation junit:./$line.xml
+
+   inspec exec $INSPEC_PROFILE -t ssh://root@$line $PORT $PASSWORD --reporter documentation junit:./$line.xml
+done
+
+
+
